3fb0049c2cc16294c79a47cd655583aa4815d73d
Greg Kempe <greg@kempe.net>
2019-05-20 11:01:53 +0200

    Initial commit

---
+++ indigo_gm/publications.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+import requests
+
+from indigo.analysis.publications.base import BasePublicationFinder
+from indigo.plugins import plugins
+
+
+@plugins.register('publications')
+class GazetteMachinePublicationFinder(BasePublicationFinder):
+    """ Publication finder that uses Gazette Machine.
+    Tries to lookup publications for any locale.
+    """
+
+    headers = {'Authorization': 'Token %s' % settings.GM['API_AUTH_TOKEN']}
+
+    def find_publications(self, params):
+        date = params.get('date')
+        number = params.get('number')
+        jurisdiction = params.get('country')
+        publication = self.get_publication(jurisdiction, params.get('name'))
+
+        if not date:
+            raise ValueError("I need at least a date to find a gazette.")
+
+        params = {
+            'date': date,
+            'jurisdiction': jurisdiction,
+        }
+        if number:
+            params['number'] = number
+        if publication:
+            params['publication'] = publication
+        headers = self.headers
+        resp = requests.get(settings.GM['API_URL'] + '/gazettes/archived/', params=params, timeout=5.0, headers=headers)
+        resp.raise_for_status()
+
+        data = resp.json().get('results', [])
+        for d in data:
+            d['trustworthy'] = True
+            d['url'] = d['download_url']
+        return data
+
+    def get_publication(self, country, name):
+        # TODO: handle publication names
+        return 'Government Gazette'
---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'VERSION')) as f:
+    version = f.read().strip()
+
+
+setup(
+    name='indigo-gm',
+    version=version,
+    description='Indigo platform plugin for integrating with Gazette Machine',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/laws-africa/indigo-gm',
+
+    # Author details
+    author='Laws.Africa',
+    author_email='greg@laws.africa',
+
+    # See https://pypi.org/classifiers/
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Legal Industry',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    packages=find_packages(exclude=['docs']),
+    include_package_data=True,
+
+    python_requires='~=2.7',
+    install_requires=[
+        'indigo>=3',
+        'requests>=2',
+    ],
+)
